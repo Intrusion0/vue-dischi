2b3e742a78f7aa700a39918bc13f53122bc3235e
Mario <lombardomario01@gmail.com>
2021-11-29 16:17:35 +0100

    Bonus completed

--- src/components/Tracks.vue
+++ src/components/Tracks.vue
@@ -1,100 +1,93 @@
 <template>
   <div class="container-tracks">
-      <!-- Componente Select Genre -->
-      <SelectGenre 
-      @changeGenre="selectGenreOption"
-      />
-      <!-- Componente SelectAuthor -- BONUS -- -->
-      <SelectAuthor
-      :details="cards"
-      @changeAuthor="SelectAuthorOption"
-      />
-      <!-- Componente Loader -->
-      <Loader v-if="contCards == false"/>
-      <!-- Componente CardTrack -->
-      <CardTrack v-else 
-      v-for="card, i in filteredAuthorCards" 
+    <!-- Componente Select Genre -->
+    <SelectGenre @changeGenre="selectGenreOption" />
+    <!-- Componente SelectAuthor -- BONUS -- -->
+    <SelectAuthor :details="cards" @changeAuthor="selectAuthorOption" />
+    <!-- Componente Loader -->
+    <Loader v-if="contCards == false" />
+    <!-- Componente CardTrack -->
+    <CardTrack
+      v-else
+      v-for="(card, i) in filteredCards"
       :key="i"
       :details="card"
-      />
+    />
   </div>
 </template>
 
 <script>
-import axios from 'axios'
-import CardTrack from './CardTrack.vue'
-import Loader from './Loader.vue'
-import SelectGenre from './SelectGenre.vue'
-import SelectAuthor from './SelectAuthor.vue'
+import axios from "axios";
+import CardTrack from "./CardTrack.vue";
+import Loader from "./Loader.vue";
+import SelectGenre from "./SelectGenre.vue";
+import SelectAuthor from "./SelectAuthor.vue";
 
 export default {
-  name: 'Traks',
+  name: "Traks",
   components: {
-      CardTrack,
-      Loader,
-      SelectGenre,
-      SelectAuthor
+    CardTrack,
+    Loader,
+    SelectGenre,
+    SelectAuthor,
   },
   data() {
-      return {
-          apiUrl: 'https://flynn.boolean.careers/exercises/api/array/music',
-          cards: [],
-          contCards: false,
-          selectedGenreOption: 'All',
-          selectedAuthorOption: 'All'
-      }
+    return {
+      apiUrl: "https://flynn.boolean.careers/exercises/api/array/music",
+      cards: [],
+      contCards: false,
+      selectedGenreOption: "All",
+      selectedAuthorOption: "All",
+    };
   },
   computed: {
-      filteredGenreCards() {
-          if (this.selectedGenreOption == 'All') {
-              return this.cards;
-          }
-          return this.cards.filter((card) => {
-              return card.genre === this.selectedGenreOption;
-          })
-      },
-      filteredAuthorCards() {
-          if (this.selectedAuthorOption == 'All') {
-              return this.cards;
-          }
-          return this.cards.filter((card) => {
-              return card.author === this.selectedAuthorOption;
-          })
+    filteredCards() {
+      let filteredCards = this.cards;
+
+      if (this.selectedGenreOption !== "All") {
+        filteredCards = filteredCards.filter((card) => {
+          return card.genre === this.selectedGenreOption;
+        });
       }
+      if (this.selectedAuthorOption !== "All") {
+        filteredCards = filteredCards.filter((card) => {
+          return card.author === this.selectedAuthorOption;
+        });
+      }
+      return filteredCards;
+    }
   },
   created() {
-      this.apiCardMusic();
+    this.apiCardMusic();
   },
   methods: {
-      apiCardMusic() {
-          axios.get(this.apiUrl)
-          .then((result) => {
-              this.contCards = true;
-              this.cards = result.data.response;
-          })
-          .catch ((error) => {
-              console.log('Attenzione!! ' + error);
-          })
-      },
-      selectGenreOption(event) {
-          this.selectedGenreOption = event.target.value;
-          console.log(event.target.value);
-      },
-      SelectAuthorOption(event) {
-          this.selectedAuthorOption = event.target.value;
-          console.log(event.target.value);
-      }
-  }
-}
+    apiCardMusic() {
+      axios
+        .get(this.apiUrl)
+        .then((result) => {
+          this.contCards = true;
+          this.cards = result.data.response;
+        })
+        .catch((error) => {
+          console.log("Attenzione!! " + error);
+        });
+    },
+    selectGenreOption(event) {
+      this.selectedGenreOption = event.target.value;
+    },
+    selectAuthorOption(event) {
+      this.selectedAuthorOption = event.target.value;
+    },
+  },
+};
 </script>
 
 <style scoped lang="scss">
 .container-tracks {
-    display: flex;
-    flex-wrap: wrap;
-    width: 65%;
-    margin: 0 auto;
-    padding: 0 10px 0 27px;
+  display: flex;
+  flex-wrap: wrap;
+  width: 65%;
+  margin: 0 auto;
+  padding: 0 10px 0 27px;
 }
-
 </style>
